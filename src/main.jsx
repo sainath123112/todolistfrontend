6320f3fefd851af3d6e857945d0b9a5b6ddd6ec9
Talluri Sainath <sainath.t31@gmail.com>
2023-11-17 15:07:01 -0600

    Worked on Home and Navbar creation and setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ContextContainer from './contextcontainer/ContextContainer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <ContextContainer>
+      <App />
+   </ContextContainer>
+ 
   </React.StrictMode>,
 )
